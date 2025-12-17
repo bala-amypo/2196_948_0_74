efcde71b00cbfecbae295068646de0643ed8ad11
bala-amypo <bala@amypo.in>
2025-12-17 10:19:32 +0000

    User commit

--- src/main/java/com/example/demo/controller/studentController.java
+++ src/main/java/com/example/demo/controller/studentController.java
@@ -2,7 +2,7 @@ package com.example.demo.controller;
 
 import jakarta.validation.Valid;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.example.demo.entity.*;
 import com.example.demo.service.*;
 
@@ -20,6 +20,6 @@ public class studentController{
 
     @PostMapping("/add")
     public studentEntity addStudent(@valid @ResquestBody studentEntity student){
-        return service.addStudent
+        return service.addStudent(student);
     }
 }
--- src/main/java/com/example/demo/server/studentService.java
+++ src/main/java/com/example/demo/server/studentService.java
@@ -1,8 +1,9 @@
 package com.example.demo.service;
 
+import com.example.demo.entity.*;
 interface studentService{
 
-
-
     List<studentEntity>getAll();
+
+    studentEntity addStudent(studentEntity student)
 }
