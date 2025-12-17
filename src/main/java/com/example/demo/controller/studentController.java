ece42520e63e674d47e5405a1c3732f7d6bc502f
bala-amypo <bala@amypo.in>
2025-12-17 09:56:02 +0000

    User commit

--- src/main/java/com/example/demo/controller/studentController.java
+++ src/main/java/com/example/demo/controller/studentController.java
@@ -1,11 +1,15 @@
-package com.example.demo.
+package com.example.demo.controller;
 
-
-
-
+import com.example.demo.entity.*;
 
 public class studentController{
 
+    @Autowired
+    studentService service;
+
     @GetMapping("/getAllStudent")
+    public List<studentEntity> getAll(){
+
+    }
 
 }
