2bf76d8badd1ff9357bfdf103fe5b600600fddac
bala-amypo <bala@amypo.in>
2025-12-18 08:42:24 +0000

    User commit

--- src/main/java/com/example/demo/server/impl/studentServiceImpl.java
+++ src/main/java/com/example/demo/server/impl/studentServiceImpl.java
@@ -21,5 +21,14 @@ public class studentServiceImpl implements studentService{
     public studentEntity addStudent(studentEntity student){
         return repo.save(student);
     }
+    public studentEntity getbyId(Long id){
+    return repo.findById(id).orElseThrow(() -> new StudentNotFoundException(errMsg: "Student ID not Found"));
+    }
+    public studentEntity updateById(Long id, studentEntity newstu){
+    studentEntity existing = getbyId(id);
+    newstu.setId(existing.getId()); // newstu.setId(id);
+    return repo.save(newstu);   
+    }
+
 
 }
